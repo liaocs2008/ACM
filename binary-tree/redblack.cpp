29209b58e4c66f70f9478c20c83f632e124e1145
Leo <liaocs2008@gmail.com>
2015-01-27 23:04:51 -0500

    init red black tree

---
+++ binary-tree/redblack.cpp
@@ -0,0 +1,130 @@
+#include <stdio.h>
+
+typedef enum {red, black} color;
+
+typedef struct node node;
+struct node {
+  int key;
+  node *p, *left, *right;
+  color c;
+};
+
+// sentinel
+node sentinel = {.key = 0, .p = NULL, .left = NULL, .right = NULL, .c = black};
+node *nil = &sentinel;
+
+void inorder_walk(node *x)
+{
+  if (nil != x) {
+    inorder_walk(x->left);
+    printf("%d, ", x->key);
+    inorder_walk(x->right);
+  }
+}
+
+void left_rotate(node *& root, node *x) // this functon may update root
+{
+  node *y = x->right;
+  // subtree belta
+  x->right = y->left; // how do you know if y is not nil
+  if (nil != y->left) y->left->p = x;
+  // x's parent
+  y->p = x->p;
+  if (nil == x->p) root = y;
+  else {
+    if (x->p->left == x) x->p->left = y;
+    else x->p->right = y;
+  }
+  // y 
+  y->left = x;
+  // x
+  x->p = y;
+}
+
+void right_rotate(node *&root, node *y) // this functon may update root
+{
+  node *x = y->left;
+  // subtree belta
+  y->left = x->right; // how do you know if x is not nil
+  if (nil != x->right) x->right->p = y;
+  // y's parent
+  x->p = y->p;
+  if (nil == y->p) root = x;
+  else {
+    if (y->p->left == x) y->p->left = x;
+    else y->p->right = x;
+  }
+  // x 
+  x->right = y;
+  // y
+  y->p = x;
+}
+
+void insert_fixup(node *root, node *z) // this functon won't update root
+{
+  node *y = nil; 
+  while (red == z->p->c) { // if parent is nil, it won't go into loop
+    if (z->p == z->p->p->left) {
+      y = z->p->right; // y is the uncle of z
+      if (red == y->c) {
+        y->c = black;
+        z->p->c = black;
+        z->p->p->c = red; // this may cause the same problem as z was, red-red
+        z = z->p->p;
+      } else { // y has black color
+        if (z == z->p->right) { // convert z to its parent's left child
+          z = z->p;
+          left_rotate(root, z);
+        }
+        z->p->c = black;
+        z->p->p->c = red;
+        right_rotate(root, z->p->p);
+      }
+    } else { // following are symmetric
+      y = z->p->p->left;
+      if (red == y->c) {
+        y->c = black;
+        z->p->c = black;
+        z->p->p->c = red;
+        z = z->p->p;
+      } else {
+        if (z == z->p->left) {
+          z = z->p;
+          right_rotate(root, z);
+        }
+        z->p->c = black;
+        z->p->p->c = red;
+        left_rotate(root, z->p->p);
+      }
+    }
+  }
+  root->c = black;
+}
+
+void insert(node *&root, node *z) // this function may update root
+{
+  node *y = nil, *x = root;
+  // find position to insert, y is always x's parent
+  while (nil != x) {
+    y = x;
+    if (z->key < x->key) x = x->left;
+    else x = x->right;
+  }
+  // insert
+  if (nil == y) root = z;
+  else {
+    if (z->key < y->key) y->left = z;
+    else y->right = z;
+  }
+  // set up z
+  z->p = y;
+  z->left = nil;
+  z->right = nil;
+  z->c = red;
+  insert_fixup(root, z);
+}
+
+int main()
+{
+  return 0;
+}
