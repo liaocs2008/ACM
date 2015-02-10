40e968aed7ebf4c2fa59076d766adc42ef3715a7
Leo <liaocs2008@gmail.com>
2015-02-10 10:40:18 -0500

    finish red-black tree

--- binary-tree/redblack.cpp
+++ binary-tree/redblack.cpp
@@ -1,4 +1,29 @@
+/*
+OUTPUT:
+   insert No.0 node, 0,
+   insert No.1 node, 0, 1,
+   insert No.2 node, 0, 1, 2,
+   insert No.3 node, 0, 1, 2, 3,
+   insert No.4 node, 0, 1, 2, 3, 4,
+   insert No.5 node, 0, 1, 2, 3, 4, 5,
+   insert No.6 node, 0, 1, 2, 3, 4, 5, 6,
+   insert No.7 node, 0, 1, 2, 3, 4, 5, 6, 7,
+   insert No.8 node, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+   insert No.9 node, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+   remove No.0 node, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+   remove No.1 node, 2, 3, 4, 5, 6, 7, 8, 9,
+   remove No.2 node, 3, 4, 5, 6, 7, 8, 9,
+   remove No.3 node, 4, 5, 6, 7, 8, 9,
+   remove No.4 node, 5, 6, 7, 8, 9,
+   remove No.5 node, 6, 7, 8, 9,
+   remove No.6 node, 7, 8, 9,
+   remove No.7 node, 8, 9,
+   remove No.8 node, 9,
+   remove No.9 node,
+ */
+
 #include <stdio.h>
+#include <assert.h>
 
 typedef enum {red, black} color;
 
@@ -147,6 +172,65 @@ void insert(node *&root, node *z) // this function may update root
   insert_fixup(root, z);
 }
 
+void remove_fixup(node *&root, node *&x) // this function may change root and x both
+{
+  node *w = nil;
+  while ((root != x) && (black == x->c)) {
+    // assume x with an extra black color
+    // we are going to remove the extra black color
+    if (x == x->p->left) {
+      w = x->p->right;
+      if (red == w->c) { // convert this case to other case
+        w->c = black;
+        x->p->c = red;
+        left_rotate(root, x->p);
+        w = x->p->right;
+      }
+
+      if ((black == w->left->c) && (black == w->right->c)) { //case 2
+        w->c = red;
+        x = x->p; // after x changes to its parent, its color will be reb, successfully finish
+      } else if (black == w->right->c) { //case 3
+        w->left->c = black;
+        w->c = red;
+        right_rotate(root, w);
+        w = x->p->right;
+      } else { //case 4
+        w->c = x->p->c; //red
+        x->p->c = black;
+        w->right->c = black;
+        left_rotate(root, x->p);
+        x = root; // successfully finish
+      }
+    } else {
+      // following are symmetric
+      w = x->p->left;
+      if (red == w->c) { 
+        w->c = black;
+        x->p->c = red;
+        right_rotate(root, x->p);
+        w = x->p->left;
+      }
+
+      if ((black == w->left->c) && (black == w->right->c)) { 
+        w->c = red;
+        x = x->p; 
+      } else if (black == w->left->c) { 
+        w->right->c = black;
+        w->c = red;
+        left_rotate(root, w);
+        w = x->p->left;
+      } else { 
+        w->c = x->p->c; 
+        x->p->c = black;
+        w->left->c = black;
+        right_rotate(root, x->p);
+        x = root; 
+      }
+    }
+  }
+}
+
 void remove(node *& root, node *z) // this function may update root
 {
   node *y = nil, *x = nil;
@@ -156,26 +240,43 @@ void remove(node *& root, node *z) // this function may update root
   // x, a child of y 
   if (nil != y->left) x = y->left;
   else x = y->right;
-  x->p = y->p; // this may change sentinel's parent, handled in fixup function
+  x->p = y->p; // WARNING: this may change sentinel's parent, handled in fixup function
   // fix y's parent
   if (nil == y->p) root = x;
   else {
     if (y == y->p->left) y->p->left = x;
     else y->p->right = x;
   }
-  if (y != z) z->key = y->key; // here, got no satellite data
-  if (black == y->c) remove_fixup();
+  // remove z, by now, y is already removed from the tree
+  if (y != z) z->key = y->key; // here, also move satellite data if existing
+  if (black == y->c) remove_fixup(root, x);
 }
 
 int main()
 {
   int i = 0;
   node a[10], *root = nil;
-  for (i = 0; i < 9; i += 1) {
+  for (i = 0; i < 10; i += 1) {
     a[i].key = i;
     insert(root, &a[i]);
+    printf("insert No.%d node, ", i);
+    inorder_walk(root);
+    printf("\n");
   }
-  inorder_walk(root);
+
+  for (i = 0; i < 10; i += 1) {
+    remove(root, &a[i]);
+    printf("remove No.%d node, ", i);
+    inorder_walk(root);
+    printf("\n");
+  }
+
+  // check if sentinel is changed
+  assert(nil->key == 0);
+  assert(nil->p == &sentinel);
+  assert(nil->left == &sentinel);
+  assert(nil->right == &sentinel);
+  assert(nil->c == black);
 
   return 0;
 }
