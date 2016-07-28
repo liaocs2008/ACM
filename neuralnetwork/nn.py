53f911ffca1ba501ee9efd87699137fec3ec2154
Leo <liaocs2008@users.noreply.github.com>
2016-07-28 16:17:13 +0800

    test on circulant
    initial test on circulant matrix, pass gradient test

--- neuralnetwork/nn.py
+++ neuralnetwork/nn.py
@@ -1,5 +1,56 @@
 import numpy as np
 
+from scipy.linalg import circulant
+
+def circulant_check():
+    for N in [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]:
+      r = np.random.random(N)
+      x = np.random.random(N)
+      d = np.fft.ifft( np.fft.fft(r) * np.fft.fft(x) ) - np.dot(circulant(r), x)
+      print N, np.mean(np.abs(d)), np.linalg.norm(d)
+    for N in [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]:
+      r = np.random.random(N)
+      x = np.random.random([N/4, N])
+      d = np.fft.ifft( np.fft.fft(x) * np.fft.fft(r) ).T - np.dot(circulant(r), x.T)
+      print N, np.mean(np.abs(d)), np.linalg.norm(d)
+
+
+class CircFC(object):
+    def __init__(self, I, H, name=None):
+        # I : input size
+        # H : hidden size
+        self.k = H
+        self.r = np.random.random( self.k )
+        self.dr = np.zeros(self.k)
+        self.mapping = np.roll(np.arange(self.k)[::-1], 1) # for shifting x
+        self.name = name
+
+    def forward(self, x):
+        a = np.fft.ifft( np.fft.fft(x) * np.fft.fft(self.r) ) # a = dot(x, R.T)
+        if __debug__:
+            check_a = np.dot(x, circulant(self.r).T)
+            assert np.linalg.norm( a - check_a ) < 1e-9
+            print self.name, "forward", x.shape, "to", a.shape
+        return a
+
+    def backward(self, x, d_a):
+        new_x = x[:, self.mapping]
+        self.dr = np.sum( np.fft.ifft(np.fft.fft(new_x)*np.fft.fft(d_a)), axis=0 )
+        # not like following
+        #d_x = np.fft.ifft(np.fft.fft(d_a)*np.fft.fft(self.r))
+        d_x = np.fft.ifft( np.fft.fft(d_a) * np.fft.fft(self.r[self.mapping]) )
+        if __debug__:
+            check_d_x = np.dot(d_a, circulant(self.r))
+            diff_norm = np.linalg.norm( d_x - check_d_x )
+            print self.name, "backward", d_a.shape, "to", d_x.shape, diff_norm
+            assert diff_norm < 1e-9
+        return d_x
+
+    def update(self, lr=0.01):
+        self.r = self.r - lr * self.dr
+        self.dr.fill(0.)
+
+
 
 class FC(object):
     def __init__(self, I, H, name=None):
@@ -190,7 +241,84 @@ def GradientChecking2():
 
     print "Finish checking fully connected"
 
+def GradientChecking3():
+    # this is to check fully connected layer
+    B = 7  # batch size
+    I = 7  # input size
+    O = I   # output size
+
+    x = np.random.random([B, I])
+    y = np.sin(x)
+    #y = np.sum(np.sin(x), axis=1).reshape(B, O)
+
+    layers = [CircFC(I, O, "CircFc1")]
+    nlayers = len(layers)
+
+    # forward and backward
+
+    # inputs[i] is the input for i-th layer
+    # the last of inputs[i] must be the output of current network
+    inputs = [x]
+    for i in xrange(nlayers):
+        inputs.append( layers[i].forward(inputs[-1]) ) # inputs[i] is the input for i-th layer
+
+    cost = EuclideanLoss()
+    # loss = cost.forward(inputs[-1], y)
+
+    # grads[i] is the gradients for i-th layer, but in the reverse order
+    grads = [cost.backward(inputs[-1], y)]
+    for i in reversed(xrange(nlayers)):
+        grads.append( layers[i].backward(inputs[i], grads[-1]) ) # grads[i]
+
+    # following checking method is from https://gist.github.com/karpathy/587454dc0146a6ae21fc
+    delta = 1e-5
+    rel_error_thr_warning = 1e-2
+    rel_error_thr_error = 1
+
+    checklist = [layers[0].r]
+    grads_analytic = [layers[0].dr]
+    names = ['r']
+    for j in xrange(len(checklist)):
+        mat = checklist[j]
+        dmat = grads_analytic[j]
+        name = names[j]
+        for i in xrange(mat.size):
+            old_val = mat.flat[i]
+
+            # test f(x + delta_x)
+            mat.flat[i] = old_val + delta
+            loss0 = fwd(x, y, layers, cost)
+
+            # test f(x - delta_x)
+            mat.flat[i] = old_val - delta
+            loss1 = fwd(x, y, layers, cost)
+
+            mat.flat[i] = old_val # recover
+
+            grad_analytic = dmat.flat[i]
+            grad_numerical = (loss0 - loss1) / (2 * delta)
+
+            if grad_numerical == 0 and grad_analytic == 0:
+                rel_error = 0 # both are zero, OK.
+                status = 'OK'
+            elif abs(grad_numerical) < 1e-7 and abs(grad_analytic) < 1e-7:
+                rel_error = 0 # not enough precision to check this
+                status = 'VAL SMALL WARNING'
+            else:
+                rel_error = abs(grad_analytic - grad_numerical) / abs(grad_numerical + grad_analytic)
+                status = 'OK'
+                if rel_error > rel_error_thr_warning: status = 'WARNING'
+                if rel_error > rel_error_thr_error: status = '!!!DANGEROUS ERROR!!!'
+
+            print '%s checking param %s index %s (val = %+8f), analytic = %+8f, numerical = %+8f, relative error = %+8f' \
+                    % (status, name, `np.unravel_index(i, mat.shape)`, old_val, grad_analytic, grad_numerical, rel_error)
+
+    print "Finish checking fully connected"
+
+
 
 if __name__ == "__main__":
     #GradientChecking1()
-    GradientChecking2()
+    #GradientChecking2()
+    #circulant_check()
+    GradientChecking3()
