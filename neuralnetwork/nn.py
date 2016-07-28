b33418dbc55ab6a7bad1ed294a519ebc4877c797
Leo <liaocs2008@users.noreply.github.com>
2016-07-28 17:56:39 +0800

    further implement circulant layer
    1. allow different size
    2. ensure returning real number

--- neuralnetwork/nn.py
+++ neuralnetwork/nn.py
@@ -19,32 +19,57 @@ class CircFC(object):
     def __init__(self, I, H, name=None):
         # I : input size
         # H : hidden size
-        self.k = H
+        self.k = max(I, H) # r is now padded
         self.r = np.random.random( self.k )
         self.dr = np.zeros(self.k)
         self.mapping = np.roll(np.arange(self.k)[::-1], 1) # for shifting x
         self.name = name
+        self.H = H # desired output size
 
     def forward(self, x):
-        a = np.fft.ifft( np.fft.fft(x) * np.fft.fft(self.r) ) # a = dot(x, R.T)
+        # a = dot(x, R.T), shape=(b, H)
+        if self.k > x.shape[1]: # then self.k = self.H
+            # pad data
+            # http://docs.scipy.org/doc/numpy/reference/generated/numpy.pad.html
+            # http://stackoverflow.com/questions/19349410/how-to-pad-with-zeros-a-tensor-along-some-axis-python
+            assert self.k == self.H
+            new_x = np.pad(x, [(0,0), (0, self.k-x.shape[1])], 'constant', constant_values=0)
+            a = np.fft.ifft( np.fft.fft(new_x) * np.fft.fft(self.r) )[:,:self.H]
+        else:
+            a = np.fft.ifft( np.fft.fft(x) * np.fft.fft(self.r) )[:, :self.H]
         if __debug__:
-            check_a = np.dot(x, circulant(self.r).T)
+            if self.k > x.shape[1]:
+                new_x = np.pad(x, [(0,0), (0, self.k-x.shape[1])], 'constant', constant_values=0)
+                check_a = np.dot(new_x, circulant(self.r).T)[:, :self.H]
+            else:
+                check_a = np.dot(x, circulant(self.r).T)[:, :self.H]
             assert np.linalg.norm( a - check_a ) < 1e-9
             print self.name, "forward", x.shape, "to", a.shape
-        return a
+        return np.real(a)
 
     def backward(self, x, d_a):
-        new_x = x[:, self.mapping]
-        self.dr = np.sum( np.fft.ifft(np.fft.fft(new_x)*np.fft.fft(d_a)), axis=0 )
-        # not like following
-        #d_x = np.fft.ifft(np.fft.fft(d_a)*np.fft.fft(self.r))
-        d_x = np.fft.ifft( np.fft.fft(d_a) * np.fft.fft(self.r[self.mapping]) )
+        if self.k > x.shape[1]:
+            # pad data, don't forget to change form of x
+            assert self.k == self.H
+            new_x = np.pad(x, [(0,0), (0, self.k-x.shape[1])], 'constant', constant_values=0)[:, self.mapping]
+            self.dr = np.sum( np.fft.ifft(np.fft.fft(new_x)*np.fft.fft(d_a)), axis=0 )
+            d_x = np.fft.ifft( np.fft.fft(d_a) * np.fft.fft(self.r[self.mapping]) )[:,:x.shape[1]]
+        else:
+            new_x = x[:, self.mapping]
+            new_d_a = np.pad(d_a, [(0,0), (0, self.k-d_a.shape[1])], 'constant', constant_values=0)
+            self.dr = np.sum( np.fft.ifft(np.fft.fft(new_x)*np.fft.fft(new_d_a)), axis=0 )
+            d_x = np.fft.ifft( np.fft.fft(new_d_a) * np.fft.fft(self.r[self.mapping]) )[:,:x.shape[1]]
         if __debug__:
-            check_d_x = np.dot(d_a, circulant(self.r))
+            if self.k > x.shape[1]:
+                check_d_x = np.dot(d_a, circulant(self.r))[:, :x.shape[1]]
+            else:
+                new_d_a = np.pad(d_a, [(0,0), (0, self.k-d_a.shape[1])], 'constant', constant_values=0)
+                check_d_x = np.dot(new_d_a, circulant(self.r))[:, :x.shape[1]]
             diff_norm = np.linalg.norm( d_x - check_d_x )
             print self.name, "backward", d_a.shape, "to", d_x.shape, diff_norm
             assert diff_norm < 1e-9
-        return d_x
+        self.dr = np.real(self.dr)
+        return np.real(d_x)
 
     def update(self, lr=0.01):
         self.r = self.r - lr * self.dr
@@ -243,15 +268,16 @@ def GradientChecking2():
 
 def GradientChecking3():
     # this is to check fully connected layer
-    B = 7  # batch size
+    B = 3  # batch size
     I = 7  # input size
+    H = 19
     O = I   # output size
 
     x = np.random.random([B, I])
     y = np.sin(x)
     #y = np.sum(np.sin(x), axis=1).reshape(B, O)
 
-    layers = [CircFC(I, O, "CircFc1")]
+    layers = [CircFC(I, H, "CircFc1"), CircFC(H, O, "CircFc2")]
     nlayers = len(layers)
 
     # forward and backward
@@ -275,10 +301,10 @@ def GradientChecking3():
     rel_error_thr_warning = 1e-2
     rel_error_thr_error = 1
 
-    checklist = [layers[0].r]
-    grads_analytic = [layers[0].dr]
-    names = ['r']
-    for j in xrange(len(checklist)):
+    checklist = [layers[0].r, layers[1].r]
+    grads_analytic = [layers[0].dr, layers[1].dr]
+    names = ['r0', 'r1']
+        for j in xrange(len(checklist)):
         mat = checklist[j]
         dmat = grads_analytic[j]
         name = names[j]
