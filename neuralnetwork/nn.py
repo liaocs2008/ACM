f21a40dfa4683cd9cad4a0f92cee572f22fff2ab
leo <liaocs2008@gmail.com>
2017-01-20 21:28:19 -0500

    update NeatCirc

--- neuralnetwork/nn.py
+++ neuralnetwork/nn.py
@@ -297,6 +297,13 @@ class NewCircFC2(object):
 
 
 
+class ShiftFC(object):
+    def __init__(self, I, H, name=None):
+        # I : input size
+        # H : hidden size
+        self.n = np.max([I, H])
+     
+
 
 
         
@@ -471,6 +478,37 @@ class Conv(object):
     def update(self, lr=0.01):
         for c in self.c:
             c.update(lr)
+
+
+
+
+# class Pooling(object):
+#     def __init__(self, (iN, iC, iW, iH), k, name=None):
+#         # (batch_size, channels, width, height)
+#         # assume stride is 1
+#         # max pooling k by k
+#         self.k = k
+#         self.name = name
+#
+#         self.indx = np.arange(iN * iC * iW * iH).reshape(-1, iW, iH)
+#         self.indx = self.indx.reshape(-1, iW/self.k, self.k, iH/self.k, self.k)
+#         self.indx = np.swapaxes(self.indx, 2, 3)
+#         self.indx = self.indx(-1, self.k * self.k)
+#
+#     def forward(self, x):
+#         assert 4 == len(x.shape)
+#         new_x = x.ravel()
+#         self.maxindx = np.argmax(new_x[self.indx], axis=0)
+#         a = x[self.maxindx]
+#
+#     def backward(self, x, d_a):
+#         d_x = np.zeros(x.shape)
+#         d_x[self.maxindx][:] = d_a.ravel()[:]
+#         return d_x
+#
+#     def update(selfself, lr=0.01):
+#         pass
+
 
 
 
@@ -1177,6 +1215,6 @@ if __name__ == "__main__":
     #GradientChecking6()
     #GradientChecking7()
     #GradientChecking8()
-    #GradientChecking9()
-    time_test2()
+    GradientChecking9()
+    #time_test2()
     pass
